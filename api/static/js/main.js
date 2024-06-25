2c60250b68a60409e872bdf54500fc9537f83cd1
Abdallah Saber <83897573+abdallah065@users.noreply.github.com>
2024-06-25 05:53:55 +0300

    chore: Refactor image preview functionality and improve code structure

--- api/static/js/main.js
+++ api/static/js/main.js
@@ -1,51 +1,53 @@
-document
-    .getElementById("cardForm")
-    .addEventListener("submit", function (event) {
-        event.preventDefault();
+window.onload = function () {
+    document
+        .getElementById("cardForm")
+        .addEventListener("submit", function (event) {
+            event.preventDefault();
 
-        // Get form values
-        const title = document.getElementById("title").value;
-        const description = document.getElementById("description").value;
-        const image_url = document.getElementById("image_url").value;
-        const url = document.getElementById("url").value;
+            // Get form values
+            const title = document.getElementById("title").value;
+            const description = document.getElementById("description").value;
+            const image_url = document.getElementById("image_url").value;
+            const url = document.getElementById("url").value;
 
-        // Create card data
-        const cardData = { title, description, image_url, url };
+            // Create card data
+            const cardData = { title, description, image_url, url };
 
-        // preview card
-        document.getElementById("previewContainer").style.visibility = "visible";
-        document.getElementById("previewLink").href = url;
-        document.getElementById("previewImage").style.width = document.getElementById("mainContainer").style.width
-        document.getElementById("previewImage").src = image_url;
+            // preview card
+            document.getElementById("previewLink").href = url;
+            document.getElementById("previewImage").style.width = document.getElementById("mainContainer").style.width
+            document.getElementById("previewImage").src = image_url;
+            document.getElementById("previewContainer").style.display = "block";
 
 
-        // Send data to the server
-        fetch("/create-card", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(cardData),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                const link = data.link;
+            // Send data to the server
+            fetch("/create-card", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(cardData),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    const link = data.link;
 
-                // Update link input with generated link
-                const linkInput = document.getElementById("cardLink");
-                linkInput.value = link;
+                    // Update link input with generated link
+                    const linkInput = document.getElementById("cardLink");
+                    linkInput.value = link;
 
-                // Show the link container
-                document.getElementById("linkContainer").style.display = "block";
-            });
-    });
+                    // Show the link container
+                    document.getElementById("linkContainer").style.display = "block";
+                });
+        });
 
-// Copy link to clipboard
-document
-    .getElementById("copyButton")
-    .addEventListener("click", function () {
-        const linkInput = document.getElementById("cardLink");
-        linkInput.select();
-        document.execCommand("copy");
-        alert("Link copied to clipboard!");
-    });
+    // Copy link to clipboard
+    document
+        .getElementById("copyButton")
+        .addEventListener("click", function () {
+            const linkInput = document.getElementById("cardLink");
+            linkInput.select();
+            document.execCommand("copy");
+            alert("Link copied to clipboard!");
+        });
+}
